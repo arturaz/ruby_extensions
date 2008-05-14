62faafa0bf633a419f7aa6c626ed609a62e76a9b
x11 <x11@a439b4bb-83d8-46c6-86ec-f6d4d22dddae>
2008-05-14 09:44:44 +0000

    Extracted as_lt_words to separate plugin.
    git-svn-id: svn://arturaz.net/public/rails/plugins/ruby_extensions/trunk@775 a439b4bb-83d8-46c6-86ec-f6d4d22dddae

--- init.rb
+++ init.rb
@@ -2,10 +2,6 @@ require "#{File.dirname(__FILE__)}/lib/arturaz/ruby_extensions.rb"
 
 Array.send(:include, Arturaz::ArrayExtensions)
 String.send(:include, Arturaz::StringExtensions)
-Time.send(:include, Arturaz::TimeExtensions::InstanceMethods)
-Time.send(:private, :ago_as_lt_words, :since_as_lt_words)
-Time.extend Arturaz::TimeExtensions::ClassMethods
-Integer.send(:include, Arturaz::IntegerExtensions)
 
 #String.send(:alias_method, :downcase_without_lithuanian_letters, :downcase)
 #String.send(:alias_method, :downcase, :downcase_with_lithuanian_letters)
